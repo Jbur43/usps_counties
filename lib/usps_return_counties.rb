f58b9bf64524dd8540b6d97c66d9ce48eb550123
Jbur43 <jtburum@gmail.com>
2016-12-05 22:24:23 -0500

    need to understanding why the url isnt working

--- lib/usps_return_counties.rb
+++ lib/usps_return_counties.rb
@@ -1,36 +1,24 @@
 # require "usps_return_counties/version"
 require 'nokogiri'
 require 'rest_client'
-require './usps_return_counties/counties_and_population'
-require './usps_return_counties/constants'
+require 'usps_return_counties/counties_and_population'
+require 'usps_return_counties/constants'
 
 USPS_URL = 'http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup'.freeze
 ROOT     = 'CityStateLookupResponse'.freeze
 
-
-# class MyGem
-#   def initialize(user_id, zip)
-#     @user_id = user_id
-#     @zip = zip
-#   end
-# end
-#   MyGem.new(user_id, zip).get_city_state(zip)
-
-#   def self.get_city_state(zip)
-#     UspsReturnCounties.get_city_state(user_id, zip)
-#   end
-# end
-
 module UspsReturnCounties
   # will need to pass in User ID here
-  def self.get_city_state_abbrv(zip)
-    api_url = url(zip)
+  def self.get_city_state_abbrv(user_id, zip)
+    api_url = url(zip, user_id)
+    puts api_url
     xml_response = RestClient.get(api_url)
     parsed_response = Nokogiri::XML(xml_response)
 
     state_abbrv = parsed_response.xpath("//State").inner_html
+    puts state_abbrv
     city = parsed_response.xpath("//City").inner_html
-    [city, state_abbrv]
+    puts [city, state_abbrv]
   rescue => ex
     { error: ex.message }
   end
@@ -51,11 +39,32 @@ module UspsReturnCounties
     CountiesAndPopulation.counties_and_population_from(state_name)
   end
 
-  def self.url(zip)
-    [USPS_URL, query_xml(zip)].join('&')
+  def self.url(zip, user_id)
+    [USPS_URL, query_xml(zip, user_id)].join('&')
   end
 
-  def self.query_xml(zip)
-    "XML=<CityStateLookupRequest USERID='167THEBO3702'><ZipCode ID='0'><Zip5>#{zip}</Zip5></ZipCode></CityStateLookupRequest>"
+  def self.query_xml(zip, user_id)
+    puts 'user id'
+    puts user_id
+    puts 'zip'
+    puts zip
+    "XML=<CityStateLookupRequest USERID=#{user_id}><ZipCode ID='0'><Zip5>#{zip}</Zip5></ZipCode></CityStateLookupRequest>"
   end
 end
+
+# 167THEBO3702
+
+module UspsReturnCounties
+  class UspsReturnCountiesInit
+    attr_reader :user_id, :zip
+
+    def initialize(user_id, zip)
+      @user_id = user_id
+      @zip = zip
+    end
+
+    get_county = UspsReturnCounties::UspsReturnCountiesInit.new('167THEBO3702', '08691')
+    UspsReturnCounties.get_city_state_abbrv(get_county.user_id, get_county.zip)
+  end
+end
+
